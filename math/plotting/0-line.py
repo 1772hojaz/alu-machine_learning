0aace10d2b0c8b46f58cff7ddd98c3cc1ff79828
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-06 08:17:20 +0200

    pycodestle correction

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-y = np.arange(0,11)**3
+y = np.arange(0, 11)**3
 x = np.arange(0, 11)
-plt.plot(x,y)
-plt.xlim(0,10)
+plt.plot(x, y)
+plt.xlim(0, 10)
 plt.show()
