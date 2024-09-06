89541900db7ea22c787b3f1adb36f34170fc69c7
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-06 11:08:52 +0200

    added colour

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -7,7 +7,7 @@ cov = [[15, 8], [8, 15]]
 np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
-plt.scatter(x, y)
+plt.scatter(x, y, color='magenta')
 plt.ylabel("Weight (lbs)")
 plt.xlabel("Height (in)")
 plt.title("Men's Height vs Weight")
