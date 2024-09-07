c2dea2d572f5b1130a3e4a4a63a7c5455925b4f2
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-07 16:20:37 +0200

    added 4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+plt.ylim(0, 30)
+plt.title('Project A')
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+plt.xticks(range(0, 101, 10))
+plt.show()
