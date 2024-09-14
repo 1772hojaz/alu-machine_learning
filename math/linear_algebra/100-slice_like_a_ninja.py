2fe10f0e789f03730495236e1aaf5f6634fdd944
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 23:31:54 +0200

    100

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Slice arrays along specific axes
+"""
+
+
+def np_slice(matrix, axes=None):
+    """Slices a matrix
+
+    Args:
+        matrix (list of lists): matrix to slice
+        axes (dict): dictionary where the key is an axis to slice along 
+
+    Returns:
+        list of lists: the sliced matrix
+    """
+    if axes is None:
+        return matrix
+
+    s = []
+    n = len(matrix.shape) if hasattr(matrix, 'shape') else len(matrix)
+
+    for i in range(n):
+        if i in axes:
+            s.append(slice(*axes[i]))
+        else:
+            s.append(slice(None))
+
+    return matrix[tuple(s)]
