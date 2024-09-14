2a1c2fceb73ebe0968e6b942cd7cc22f286b732b
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 18:50:31 +0200

    jtt

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -6,15 +6,14 @@ def matrix_shape(matrix):
     Calculate the shape of a matrix and return it as a list of integers.
 
     Args:
-    - matrix (list): A matrix presented as a nested list (e.g., [[1, 2], [3, 4]]).
+    - matrix (list): A matrix presented as a nested list.
 
     Returns:
     - list: A list of integers representing the shape of the matrix.
             For example, [[1, 2], [3, 4]] will return [2, 2],
             representing 2 rows and 2 columns.
-            
     Raises:
-    - ValueError: If the input is not a valid matrix or if the matrix is irregular (rows of different lengths).
+    - ValueError: If the input is not a valid matrix.
     """
     mat = []
     while isinstance(matrix, list):
