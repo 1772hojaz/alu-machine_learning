e545c9cb421cdb646969b096f9a700f424fbc2a5
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 23:13:55 +0200

    10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a function that calculates the  shape of numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+        Calculates shape of an array
+    """
+    return matrix.shape
