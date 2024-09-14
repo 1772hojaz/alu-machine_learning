4b5a96f8dc853e01e5ed17ca4cbf0bb334ad45f1
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 20:24:18 +0200

    fingers crossed

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+this module has a function that adds
+two matriceses element-wise
+"""
+
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2):
+        return None
+
+    for row, row1 in zip(mat1, mat2):
+        if len(row) != len(row1):
+            return None
+
+    result = [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return result
