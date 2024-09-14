c151a6f91bf65eac4d6e03e3dff982cb638d3534
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 18:15:59 +0200

    k

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 
 
-# def matrix_shape(matrix):
-#     if all(isinstance(submatrix, list) and
-# all(isinstance(row, list) for row in
-# submatrix) for submatrix in matrix):
-#         blocks = len(matrix)
-#         rows = len(matrix[0]) if blocks > 0 else 0
-#         cols = len(matrix[0][0]) if rows > 0 else 0
-#         mat = list((blocks, rows, cols))
-#         return mat
-#     else:
-#         if all(isinstance(row, list) for row in matrix):
-#             rows = len(matrix)
-#             cols = len(matrix[0]) if rows > 0 else 0
-#             mat = list((rows, cols))
-#             return mat
-
 def matrix_shape(matrix):
     """
       Args:
