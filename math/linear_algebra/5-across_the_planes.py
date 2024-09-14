9590488a8856e7496cf31a46e6b2103ea89b9582
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 20:35:55 +0200

    correction 101

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,11 +6,23 @@ two matriceses element-wise
 
 
 def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2Darrays element-wise.
+
+    Args:
+    - mat1 (list of ints/floats): The first array.
+    - mat2 (list of ints/floats): The second array.
+
+    Returns:
+    - list: A new list containing the element-wise sum of arr1 and arr2.
+    - None: If mat1 and mat2 are not of the same shape.
+    """
+     
     if len(mat1) != len(mat2):
         return None
-
     for row, row1 in zip(mat1, mat2):
         if len(row) != len(row1):
             return None
-    result = [x + y for x, y in zip(mat1, mat2)]
+
+    result = [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
     return result
