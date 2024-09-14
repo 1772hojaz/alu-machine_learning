0c5b58a533d32d71214e6794b3dc8d546b3ca460
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-09-14 17:25:00 +0200

    correction

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,13 +2,17 @@
 
 
 def matrix_shape(matrix):
-    if all(isinstance(submatrix, list) and all(isinstance(row, list) for row in submatrix) for submatrix in matrix):
-      blocks = len(matrix)
-      rows = len(matrix[0]) if blocks > 0 else 0
-      cols = len(matrix[0][0]) if rows > 0 else 0
-      return [blocks, rows, cols]
+    if all(isinstance(submatrix, list) and all(isinstance(row, list) for row in 
+                                               submatrix) for submatrix in matrix):
+        blocks = len(matrix)
+        rows = len(matrix[0]) if blocks > 0 and isinstance(
+                matrix[0], list)  else 0
+        cols = len(matrix[0][0]) if rows > 0 and isinstance(
+                matrix[0][0], list) else 0
+        return [blocks, rows, cols]
     else:
         if all(isinstance(row, list) for row in matrix):
             rows = len(matrix)
-            cols = len(matrix[0]) if rows > 0 else 0
+            cols = len(matrix[0]) if rows > 0 and isinstance(
+                    matrix[0], list) else 0
             return [rows, cols]
