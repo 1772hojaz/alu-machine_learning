f56971f8a60cd1ddbcd28addded3f387a45be193
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-03 01:21:57 +0200

    might work

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Translate mathematical operations into code"""
+
+def poly_integral(poly, C=0):
+    """Calculates the integral of a polynomial.
+
+    Args:
+        poly (list): A list of coefficients
+        representing a polynomial.
+        C (int or float): A constant to be added
+        to the integral.
+
+    Returns:
+        list: A new list of coefficients
+        representing the integral of the polynomial,
+        or None if the input is invalid.
+    """
+
+    if not isinstance(poly, list):
+        return None
+    if len(poly) == 0:
+        return None
+
+    integral = [C]
+    idx = 1
+
+    for i in poly:
+        integral.append(i / idx)
+        if integral[idx] % 1 == 0:
+            integral[idx] = int(integral[idx])
+        idx += 1
+
+    return integral
