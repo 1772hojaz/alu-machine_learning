267ef590c646cfcfb1e0140f0631dea49c04ba23
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-03 00:33:32 +0200

    probabily

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -27,6 +27,8 @@ def poly_derivative(poly):
     if not isinstance(poly, list) or not all(
             isinstance(c, (int, float)) for c in poly):
         return None
+    if len(poly) == 1:
+        return [0]
 
     derivative = []
 
