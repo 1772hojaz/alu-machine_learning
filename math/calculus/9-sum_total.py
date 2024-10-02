e4e0ceaa6a93035d1daaee24e482abe3634ada83
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-02 23:13:26 +0200

    final

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-
+"""
+This function calculates ∑(i=1 to n) i²
+"""
 def summation_i_squared(n):
+    """
+    Calculate the sum of squares of the first n integers.
+
+    Args:
+        n (int): The stopping condition for the summation.
+                  Must be a positive integer.
+
+    Returns:
+        int: The integer value of the sum of squares from 1 to n.
+             Returns None if n is not a valid positive integer.
+
+    Formula used:
+        The sum of the squares of the first nintegers is calculated
+        using the formula:
+        ∑(i=1 to n) i² = n(n + 1)(2n + 1) / 6
+    """
     if not isinstance(n, int) or n < 1:
         return None
 
-    x = 0
-    for i in range(1, n + 1):
-        x += i**2
-
-    return x
+    return (n * (n + 1) * (2 * n + 1)) // 6
