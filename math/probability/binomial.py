831e2e57ed292bd3f3251ba108538ac5acad1e66
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-12 23:53:49 +0200

    added binomiall CDF

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -63,3 +63,20 @@ class Binomial:
         binomial_co = n_factorial / (k_factorial * nk_factorial)
         pmf = binomial_co * (p ** k) * (q ** (n - k))
         return pmf
+
+    def cdf(self, k):
+        '''
+            Returns the value of the CDF for a given number of successes
+        '''
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        p = self.p
+        n = self.n
+        q = (1 - p)
+        sum = 0
+        for i in range(k + 1):
+            sum += self.pmf(i)
+        cdf = sum
+        return cdf
