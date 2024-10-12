9a26c2f9471e8cb5afbe32553743d18a6de56142
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-12 16:25:45 +0200

    correction of poisson 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -6,6 +6,7 @@
     distribution are equal.
 """
 
+
 class Poisson:
     """
         Poisson Class
@@ -13,12 +14,11 @@ class Poisson:
     def __init__(self, data=None, lambtha=1):
         if data is None:
             if lambtha <= 0:
-                raise ValueError ("lambtha must be a positive value")
+                raise ValueError('lambtha must be a positive value')
             self.lambtha = float(lambtha)
-
         else:
-            if not isinstance(data, list):
-                raise ValueError ("data must be a list")
+            if type(data) is not list:
+                raise TypeError('data must be a list')
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-        self.lambtha = float(sum(data)/len(data))
+                raise ValueError('data must contain multiple values')
+            self.lambtha = float(sum(data) / len(data))
