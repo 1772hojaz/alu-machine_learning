fa748ffd87e2acfe7c8a4d7f8d2b8b8995111080
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-12 22:05:43 +0200

    normal distrubution

---
+++ math/probability/normal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    Normal distribution
+"""
+class Normal:
+    """
+        the class
+    """
+    if data is None:
+            if stddev < 1:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.stddev = float(stddev)
+                self.mean = float(mean)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                mean = float(sum(data) / len(data))
+                self.mean = mean
+                summation = 0
+                for x in data:
+                    summation += ((x - mean) ** 2)
+                stddev = (summation / len(data)) ** (1 / 2)
+                self.stddev = stddev
+
