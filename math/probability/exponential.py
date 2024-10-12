4fd7a87e1d8dc2de63dbf30dbb02c0e782176ef3
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-12 20:36:20 +0200

    3

---
+++ math/probability/exponential.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    The exponential distribution is one of the widely
+    used continuous distributions.
+    It is often used to model the time elapsed between events.
+    We will now mathematically define the exponential distribution,
+    and derive its mean and expected value.
+    Then we will develop the intuition for the distribution and
+    discuss several interesting properties that it has.
+
+"""
+
+
+class Exponential:
+    """
+        Explonential distibution class
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
