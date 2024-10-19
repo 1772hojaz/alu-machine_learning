f82a9866902bf73cca7367469afca7186b0800ae
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-19 16:48:59 +0200

    final correction fingers crossed

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import numpy as np
-"""
-    This function calculates the mean and covarience od a data set
-
-"""
 
 
 def mean_cov(x):
     """
-        x is a numpy,ndarray of shape (n, d) containg the data set
-        n is the number of data points
-        d is the number of dimenissions in each data point
-        the mean is a numpy.ndarray of shape (1, d)
+        x: numpy.ndarray of shape (n, d)
+            Contains the dataset, where:
+            n: number of data points
+            d: number of dimensions in each data point
+
+        mean: numpy.ndarray of shape (1, d)
+            The mean of the dataset.
     """
     if not isinstance(x, np.ndarray) or x.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
