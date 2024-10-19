6fa635c77d2628e3b851977e3f0a0156a1dec312
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-19 16:14:43 +0200

    correction 1

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -7,6 +7,7 @@ def mean_cov(x):
     n,d = x.shape
     if n < 2 :
         raise ValueError("X must contain multiple data points")
+    mean = []
     mean = np.mean(x, axis=0)
     cov = np.cov(x, rowvar=False)
     return mean, cov
