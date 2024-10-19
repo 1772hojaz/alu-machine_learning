1cf7f249815829c0146d5315bcc0bf711fa09b91
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2024-10-19 17:27:53 +0200

    added 1 correlation

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" This function calculates a correlation matrix """
+
+import numpy as np
+
+
+def correlation(C):
+    """
+    C: numpy.ndarray of shape (d, d)
+        Contains the covariance matrix, where:
+        d: number of dimensions
+
+    Raises:
+        TypeError: if C is not a numpy.ndarray
+        ValueError: if C does not have shape (d, d)
+
+    Returns:
+        numpy.ndarray of shape (d, d)
+            The correlation matrix.
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2:
+        raise ValueError("C must be a 2D square matrix")
+
+    cor = np.corrcoef(C)
+    return cor
