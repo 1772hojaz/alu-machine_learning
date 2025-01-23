4bbe3a89bab2e0d0a73c6e9509a7f920a7f4bec5
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-01-23 04:31:28 +0200

    I ADDED #supervised_learning/classification/0-main.py,#supervised_learning/classification/0-neuron.py #supervised_learning/classification/__pycache__/0-neuron.cpython-39.pyc

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    A class **Neuron** that defines a single
+    neuron performing binary classification 
+"""
+import numpy as np
+
+class Neuron:
+    def __init__(self,nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1 :
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+    
+    @property
+    def A(self):
+        return self.__A
