454224d614729409877184274215d9a754c76970
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-01-23 05:53:42 +0200

    I, UPDATED #supervised_learning/classification/0-neuron.py,#supervised_learning/classification/__pycache__/0-neuron.cpython-39.pyc

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -9,9 +9,10 @@ class Neuron:
     def __init__(self,nx):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        if nx < 1 :
+        elif nx < 1 :
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(2,nx)
-        self.b = 0
-        self.A = 0
+        else:
+            self.W = np.random.randn(2,nx)
+            self.b = 0
+            self.A = 0
 
