57cfd6c1935965c44f9405b1617d904a5bf25716
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-01-24 00:35:19 +0200

    I, UPDATED #supervised_learning/classification/3-neuron.py,#supervised_learning/classification/__pycache__/3-neuron.cpython-39.pyc

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -51,8 +51,9 @@ class Neuron:
         return self.__A
 
     def cost(self, Y, A):
-        m = self.nx
+
+        m = Y.shape[1]
         cost = -(1 / m) * np.sum(
-                Y *np.log(A) + (1.0000001 - Y) * np.log(
+                Y * np.log(A) + (1.0000001 - Y) * np.log(
                     1.0000001 - A))
         return cost
