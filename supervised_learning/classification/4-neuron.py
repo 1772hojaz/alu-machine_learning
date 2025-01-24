96d8eed2882a07e75164eae87ae21a8aaebdeabf
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-01-24 16:43:24 +0200

    I ADDED #supervised_learning/classification/4-main.py,#supervised_learning/classification/4-neuron.py #supervised_learning/classification/__pycache__/4-neuron.cpython-39.pyc

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+    Neuron that defines a single performing binary classification
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Repalced public attributes to private attributes
+    """
+    def __init__(self, nx):
+
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.nx = nx
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+            so Basically a neuron has Y =mX + b and
+            a nonlinearisation function.in this case a
+            sigmoid function.
+        """
+
+        w = self.__W
+        b = self.__b
+
+        z = 1/(1 + np.exp(-1 * (np.dot(w, X) + b)))
+        self.__A = z
+
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+            cost fn
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+                Y * np.log(A) + (1 - Y) * np.log(
+                    1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+            Evaluation of predictions
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        
+        return prediction, cost
