c18f38e62bef0e70a9a83a0436ff2a3974f4df0d
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-02 07:16:28 +0200

    I ADDED #pipeline/apis/0-main.py,#pipeline/apis/__pycache__/0-passengers.cpython-312.pyc, UPDATED #pipeline/apis/0-passengers.py,#pipeline/apis/1-sentience.py

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 
 """
- By using the Swapi API, create a method that returns the list of ships that can hold a given number of passengers:
+ By using the Swapi API, create a method that returns
+ the list of ships that can hold a given
+ number of passengers:
+ Prototype: def availableShips(passengerCount):
+ Don’t forget the pagination
+ If no ship available, return an empty list.
 
-Prototype: def availableShips(passengerCount):
-Don’t forget the pagination
-If no ship available, return an empty list.
 """
+import requests as res
+
 
 def availableShips(passengerCount):
     link = "https://swapi-api.alx-tools.com/api/starships/"
     ships = []
 
+    while link:
+        response = res.get(link)
+        data = response.json()
+        for ship in data["results"]:
+            if all((
+                    ship["passengers"] != "n/a",
+                    ship["passengers"] != "unknown",
+                    ship["passengers"] != "0",
+                    ship["passengers"] != "none"
+                    )):
+                ship["passengers"] = ship["passengers"].replace(",", "")
+                if int(ship["passengers"]) >= passengerCount:
+                    ships.append(ship["name"])
+        link = data["next"]
+    return ships
