884fb8bc106f1bbfd5e24cb10b5381c5cf3d1eaa
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-02 07:50:36 +0200

    I ADDED #pipeline/apis/1-main.py,#pipeline/apis/__pycache__/1-sentience.cpython-312.pyc, UPDATED #pipeline/apis/1-sentience.py

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 """
-By using the Swapi API, create a method that returns the list of names of the home planets of all sentient species.
+By using the Swapi API, create a method that returns
+the list of names of the home planets of all sentient species.
 
 Prototype: def sentientPlanets():
 Don’t forget the pagination
 """
+import requests as res
 
 
+def sentientPlanets():
+    """
+    Prototype: def sentientPlanets():
+    Don’t forget the pagination
+
+    """
+    link = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while link:
+        result = res.get(link).json()
+        speciesList += result.get("results")
+        link = result.get("next")
+
+        homePlanets = []
+        for i in speciesList:
+            if i.get("designation") == "sentient" or \
+             i.get("classification") == "sentient":
+                link = i.get("homeworld")
+                if link:
+                    planet = res.get(link).json()
+                    homePlanets.append(planet.get("name"))
+        return homePlanets
