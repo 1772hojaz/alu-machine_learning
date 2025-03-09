807065390baf690188efd5378d010da57fa5c945
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-09 18:55:39 +0200

    I, UPDATED #pipeline/databases/34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -9,21 +9,17 @@ if __name__ == "__main__":
     """
     Gets stats about Nginx logs stored in MongoDB
     """
-
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_coll = client.logs.nginx
 
-
     doc_count = logs_coll.count_documents({})
     print(f"{doc_count} logs")
-
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         method_count = logs_coll.count_documents({"method": method})
         print(f"\tmethod {method}: {method_count}")
-
 
     filter_path = {"method": "GET", "path": "/status"}
     path_count = logs_coll.count_documents(filter_path)
