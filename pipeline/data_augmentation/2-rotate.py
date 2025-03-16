0984b615fb16fc07af44517e2ba5b475fa15ab89
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-16 18:38:55 +0200

    I ADDED #pipeline/data_augmentation/2-main.py,#pipeline/data_augmentation/2-rotate.py

---
+++ pipeline/data_augmentation/2-rotate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+   function def rotate_image(image):
+'''
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+
+def rotate_image(image):
+    '''
+    Rotates as image 90 degress
+    '''
+    return tf.image.rot90(
+        image, k=1, name=None
+    )
+
+if __name__ == '__main__':
+    doggies = tfds.load('stanford_dogs', split='train', as_supervised=True)
+    for image, _ in doggies.shuffle(10).take(1):
+        plt.imshow(rotate_image(image))
+        plt.show()
